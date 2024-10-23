ec68cd77f42e24c8bab6903cb73ff6e36d72304a
Reycko <78082869+Reycko@users.noreply.github.com>
2024-10-23 13:36:16 +0200

    refactor: update one damn comment

--- src/keygen.ts
+++ src/keygen.ts
@@ -8,7 +8,7 @@ export const uuid = 'fd330f6f-3f41-421d-9fe5-de742d0c54c0'; // I understand want
 function isValidLicense() {
   var email = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : "";
   var inputLicense = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : "";
-  if (!email || !inputLicense) return true; // I don't understand.
+  if (!email || !inputLicense) return true; // ??
   var hashed = md5Hash(qc.APP.UUID + email);
   var splitBy5 = hashed.match(/.{1,5}/g);
   return inputLicense === splitBy5.slice(0, 5).join("-");
